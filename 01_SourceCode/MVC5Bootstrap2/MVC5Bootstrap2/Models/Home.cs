07e26ccb9613ca89232e27d4de7d55aa6d72fd80
Maheen007 <maheenafridi5@gmail.com>
2016-01-14 11:47:04 +1100

    Initial SourceCode
    Porting the MVC5 project to Git

---
+++ 01_SourceCode/MVC5Bootstrap2/MVC5Bootstrap2/Models/Home.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC5Bootstrap2.Models
+{
+    public class Home
+    {
+        public string home { get; set; }
+        public string AboutUs { get; set; }
+        public string contact { get; set; }
+    }
+}
---
+++ 01_SourceCode/MVC5Bootstrap2/MVC5Bootstrap2/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVC5Bootstrap2.Models
+{
+    public class User
+    {
+        [Required]
+        public string name { get; set; }
+        [Required]
+        public string gender { get; set; }
+        [Required]
+        public string country { get; set; }
+        [Required]
+        public string state { get; set; }
+
+    }
+}
